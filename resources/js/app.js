ab5074266222b8d986af8b35da4b33cdb8919b4e
Jose Carranco <jcarranco@tvi-mp3.com>
2021-09-09 18:20:36 -0500

    add shell

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Test } from "./Test";
+const Application = () => {
+
+    return (
+       
+            <Test />
+       
+    );
+};
+
+ReactDOM.render(<Application />, document.getElementById("root"));
